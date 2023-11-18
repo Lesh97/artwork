ea5f4076f56846e88a33a379274736bd92bb081f
Suhyeon Lee <114565244+Lesh97@users.noreply.github.com>
2023-11-19 01:22:53 +0900

    스크립트 수정

---
+++ server/src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static("build"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/build/index.html");
+});
+
+app.listen(3000, () => {
+  console.log("server is running !");
+});
+
+//클라이언트 폴더 만들기 -> npm ci -> npm run build
+
+//클라이언트 빌드 후 -> 서버로 빌드폴더 옮기기 -> 서버폴더에서 npm ci
+
+//tsc 트랜스파일링 -> node app.js
+
+// 위 과정을 스크립트로 만들어서 자동 실행하게끔
