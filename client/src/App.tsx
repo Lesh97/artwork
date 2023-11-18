055824f0c7131898ee0c78f4cc2686e5e3b5ca46
Suhyeon Lee <114565244+Lesh97@users.noreply.github.com>
2023-11-19 02:42:59 +0900

    css 파일경로 오타수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import "./App.css";
 import Header from "./components/Header";
-import Main from "./routes/main";
-import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
